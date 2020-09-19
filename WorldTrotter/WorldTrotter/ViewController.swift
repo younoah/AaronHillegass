9aa09d927e04191cbfa923260943b463dc641d81
younoah <kr_uno_10@naver.com>
2020-09-20 00:22:13 +0900

    add app WorldTrotter

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by uno on 2020/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
